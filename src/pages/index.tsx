5fd30592c02ed4c7cee884efb9ac70c70caa74aa
Sean Cooper <scooper@adaptavist.com>
2024-06-12 13:34:59 +0100

    demonstate full cache invalidation works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import {useTestMode} from "@/context/salable/use-test-mode";
 import useSWR from "swr";
 import {ResponseData} from "@/pages/api/get-data";
 
 export default function Home() {
-    const {isTestMode} = useTestMode();
     const {data, error, isLoading} = useSWR<ResponseData>('/api/get-data');
 
     if (error || !data) return <div>Failed to load</div>;
@@ -12,7 +10,6 @@ export default function Home() {
     return (
         <main>
             <h1 className='font-bold mb-4'>Toggle test mode with SWR full cache invalidation</h1>
-            <p>isTestMode Client Side State: {String(isTestMode)}</p>
             <p>isTestMode Server Side Response: {String(data.isTestMode)}</p>
         </main>
     );
